624e5701d0bc99756372d39e850e6f781eb5ba72
Aviel Shtern <aviel05484@gmail.com>
2021-10-29 22:14:22 +0300

    implement IDFT and toy test

--- sol2.py
+++ sol2.py
@@ -7,8 +7,29 @@ def DFT(signal):
     :param signal: rray of dtype float64 with shape (N,) or (N,1)
     :return: array of dtype complex128 with the same shape of input
     """
-    N = signal.shape[0] # num of sumples
-    range_N = np.arange(N)
-    vandermonde_mat = np.exp((-2j * np.pi / N ) * (range_N.reshape(N,1) * range_N))
-    return vandermonde_mat @ signal
+    return vandermonde_mat(signal.shape[0], -1, 1) @ signal
 
+
+def IDFT(fourier_signal):
+    """
+    transform a Fourier representation of 1D discrete signal back to original signal
+    :param fourier_signal: array of dtype complex128 with shape (N,) or (N,1)
+    :return: array of dtype complex128 with the same shape of input
+    """
+    return vandermonde_mat(fourier_signal.shape[0], 1, fourier_signal.shape[0]) @ fourier_signal
+
+
+def vandermonde_mat(N, type, divider):
+    """
+    :param N: num sumples
+    :param type: 1 or -1
+    :param divider: 1 or N
+    :return:
+    """
+    range_N = np.arange(N)
+    return np.exp((type * 2j * np.pi / N) * (range_N.reshape(N, 1) * range_N)) / divider
+
+
+if __name__ == '__main__':
+    SIGNAL = np.arange(20)
+    print(np.allclose(SIGNAL,IDFT(DFT(SIGNAL)).real))
