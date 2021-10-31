df782a8cd49889a75dd445f85b8fcc4f05d0de2f
Aviel Shtern <aviel05484@gmail.com>
2021-11-01 00:46:49 +0200

    end of the day. implement resize_vocoder

--- sol2.py
+++ sol2.py
@@ -130,7 +130,7 @@ def resize(data, ratio):
     dft_shifted = fftshift(DFT(data))
     before = int(num_to_add_or_reduce/ 2)
     end = int(np.ceil(num_to_add_or_reduce / 2))
-    menipulate_dft_shifted = np.pad(dft_shifted, (before, end), 'constant', constant_values=(0)) if ratio < 0 \
+    menipulate_dft_shifted = np.pad(dft_shifted, (before, end), 'constant', constant_values=(0)) if ratio < 1 \
                      else dft_shifted[before: -end] if ratio > 1 else dft_shifted
 
     return IDFT(ifftshift(menipulate_dft_shifted))
@@ -145,18 +145,34 @@ def resize_spectrogram(data, ratio):
     """
     stft_of_data = stft(data).T # stft return a metrix with shape (win_length,n_frames) ??? why every "row" and not col??
     data_menipulate = np.array([resize(stft_of_data[row], ratio) for row in range(stft_of_data.shape[0])]).T
-    return istft(data_menipulate, int(stft_of_data[0].shape[0] * 1/ratio)) # change the window size????
+    ret_val = istft(data_menipulate, int(stft_of_data[0].shape[0] * 1/ratio)) # change the window size????
+    # ret_val = istft(data_menipulate)
+    return ret_val
 
 
+def resize_vocoder(data, ratio):
+    """
+    speedups a WAV file by phase vocoding its spectrogram
+    :param data: a 1D ndarray of dtype float64 representing the original sample points
+    :param ratio: a positive float64 representing the rate change of the WAV file
+    :return: return the given data rescaled according to ratio with the same datatype as data.
+    """
+    return istft(phase_vocoder(stft(data),ratio))
 
 
 if __name__ == '__main__':
-    rate, data = read("/Users/avielshtern/Desktop/third_year/IMAGE_PROCESSING/EX/EX2/disco_dancing.wav")
+    path = "/Users/avielshtern/Desktop/third_year/IMAGE_PROCESSING/EX/EX2/ex2_presubmit/aria_4kHz.wav"
+    rate,data = read(path)
+    data = resize_spectrogram(data,2)
+    write("res.wav",rate,data.real)
+    # rate, data = read("/Users/avielshtern/Desktop/third_year/IMAGE_PROCESSING/EX/EX2/disco_dancing.wav")
+    # write("new.wav",rate,data[:,0])
+    # write("new.wav",rate,resize_spectrogram(data[:,0],2).real)
     # data_to_work = data[:2000,0]
     # print(data_to_work.shape)
     # stft_data = stft(data_to_work)
     # print(stft_data.shape)
     # istft_data  = istft(stft_data)
     # print(istft_data.shape)
-    resize_spectrogram(data[:,0],2)
+    # resize_spectrogram(data[:,0],2)
 
